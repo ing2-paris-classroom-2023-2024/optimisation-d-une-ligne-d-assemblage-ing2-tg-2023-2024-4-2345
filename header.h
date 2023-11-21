01191eaedf96d0f475d2dd1cfc536cbafb50a5e7
Erel Rebot <erel.rebot@gmail.com>
2023-11-21 15:01:27 +0100

    Ajouts des structure pour gréer un graphe dans le header.h

--- header.h
+++ header.h
@@ -4,6 +4,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Structure d'un arc*/
+struct Arc
+{
+    int sommet; // numéro de sommet d'un arc adjacent au sommet initial
+    int valeur;
+    struct Arc* arc_suivant;
+};
 
+/* Alias de pointeur sur un Arc */
+typedef struct Arc* pArc;
+
+/* Structure d'un sommet*/
+struct Sommet
+{
+    struct Arc* arc;
+    int valeur;
+    char couleur;
+
+};
+
+/* Alias de pointeur sur un Sommet */
+typedef struct Sommet* pSommet;
+
+/* Alias d'un Graphe */
+typedef struct Graphe
+{
+    int taille;
+    int orientation;
+    int ordre;
+    pSommet* pSommet;
+} Graphe;
 
 #endif // HEADER_H_INCLUDED
