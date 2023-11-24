e21216b00b1f6ab858d3b80bdbfbb69cdd955198
Erel Rebot <erel.rebot@gmail.com>
2023-11-24 18:37:09 +0100

    Supression de code inopérationnel

--- repartition.c
+++ repartition.c
@@ -4,16 +4,5 @@
 
 //Cet fonction répartie les actions dans les usines par rapport à l'exclusion.
 void repartition_2_1(Graphe* g){
-    Usine * U = (Usine *)malloc(sizeof(Usine));
-    int utilise[g->ordre];
-//Création d'un tableau vec tout les point qui vont plus tatrd être remplacé par -1 quand les points seront utilisé.
-    for (int i = 0; i < g->ordre; ++i) {
-        utilise[i] = g->pSommet[i]->valeur;
-    }
 
-    U->pSommet = (pSommet)malloc(taille_fichier("../operations.txt") * sizeof(struct Sommet));
-
-    for (int i = 0; i < g->ordre; ++i) {
-
-    }
 }
