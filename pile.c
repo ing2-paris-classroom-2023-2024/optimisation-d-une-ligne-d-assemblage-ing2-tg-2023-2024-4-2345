65558e911facf331445202b513443bdd4d1b8236
tahar-ayadi <131825020+tahar-ayadi@users.noreply.github.com>
2023-12-02 20:47:21 +0100

    Create pile.c

---
+++ pile.c
@@ -0,0 +1,35 @@
+#include "header.h"
+Pile* creerPile(int capacite) {
+    Pile* pile = (Pile*)malloc(sizeof(Pile));
+    pile->capacite = capacite;
+    pile->top = -1;
+    pile->items = (int*)malloc(sizeof(int) * capacite);
+    return pile;
+}
+
+bool estPileVide(Pile* pile) {
+    return pile->top == -1;
+}
+
+void empiler(Pile* pile, int item) {
+    pile->items[++pile->top] = item;
+}
+
+int depiler(Pile* pile) {
+    if (!estPileVide(pile)) {
+        return pile->items[pile->top--];
+    }
+    return -1;
+}
+int sommetEnHaut(Pile* pile) {
+    if (!estPileVide(pile)) {
+        return pile->items[pile->top];
+    }
+    return -1;
+}
+void afficherPile(Pile* pile) {
+    for (int i = 0; i <= pile->top; i++) {
+        printf("%d ", pile->items[i] + 1);
+    }
+    printf("\n");
+}
